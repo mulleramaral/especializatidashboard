5a1e58bc3c7fa850233427956fd1dd68d6ef5124
miiller caetano do amaral <miilleramaral@gmail.com>
2017-03-27 20:02:40 -0300

    Dashboard content

--- index.php
+++ index.php
@@ -69,25 +69,15 @@
             <a href="" class="bred">Home ></a><a href="" class="bred">Dashboard</a>
         </div>
 
-        <div class="title-pg">
-            <h1 class="title-pg">Dashboard</h1>
-        </div>
+        <?php
+        if(!isset($_GET['pag']))
+            include 'pags/home.php';
+        elseif( file_exists("pags/{$_GET['pag']}.php"))
+            include "pags/{$_GET['pag']}.php";
+        else
+            include 'pags/erro404.php';
+        ?>
 
-        <div class="content-din">
-
-            <?php for ($i = 1 ; $i <= 10 ; $i++) {?>
-            <div class="col-md-3 col-sm-4 col-xm-12">
-                <div class="rel-dash">
-                    <i class="fa fa-home" aria-hidden="true"></i>
-                    <div class="text-rel">
-                        <h2 class="result">12</h2>
-                        <h3 class="result-ds">Total de Usuários</h3>
-                    </div>
-                </div>
-            </div>
-            <?php }?>
-
-        </div>
     </div><!-- End Content DS
 
 </section> <!-- End Content -->
